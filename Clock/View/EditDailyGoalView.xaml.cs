fbc5865e24e30ddd7b652b131ce5304d363d5f9a
Bi Hoc Code <tdhung41204@gmail.com>
2024-01-13 16:02:40 +0700

    basic combobox

---
+++ Clock/View/EditDailyGoalView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Clock.View
+{
+    /// <summary>
+    /// Interaction logic for EditDailyGoalView.xaml
+    /// </summary>
+    public partial class EditDailyGoalView : UserControl
+    {
+        public EditDailyGoalView()
+        {
+            InitializeComponent();
+        }
+
+        private void PickTime_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //if (pickTimeCbox.SelectedIndex == 0)
+            //{
+            //    pickTimeCbox.Text = string.Empty;
+            //}
+        }
+    }
+}
